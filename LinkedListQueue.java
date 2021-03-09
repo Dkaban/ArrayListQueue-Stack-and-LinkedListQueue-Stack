af1727dc899d3b641fda80d69536fbbffa361a11
Dustin <dustinkaban@gmail.com>
2021-03-09 14:45:51 -0800

    Update LinkedListQueue.java

--- LinkedListQueue.java
+++ LinkedListQueue.java
@@ -2,9 +2,7 @@
 //  LinkedListQueue.java
 //
 //  AUTHOR: DUSTIN KABAN
-//  ID: T00663749
 //  DATE: March 09, 2021
-//  COURSE: COMP 2231, ASSIGNMENT 3: QUESTION 3
 //
 //  This class implements a stack using java.util.LinkedList
 //*************************************************************************************
